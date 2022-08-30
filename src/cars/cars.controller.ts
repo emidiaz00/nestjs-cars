fd9be80091302d2c486ab78a01cec7cee71e0d06
emidiaz00 <emildsn00@gmail.com>
2022-08-29 21:13:57 -0300

    agrego metodo para obtener id del car

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Param } from '@nestjs/common';
+
+
+@Controller('cars')
+export class CarsController {
+  private cars = ['Toyota', 'Volkswagen', 'Peugeot'];
+
+  @Get()
+  getAllcars() {
+    return this.cars;
+  }
+  @Get(':id')
+  getCarById(@Param('id') id) {
+    return {
+        id
+    }
+  }
+}
