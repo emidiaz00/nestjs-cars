ec83fd44cc94f1c6b4a7c5766b5db7eb6d1ab99e
emidiaz00 <emildsn00@gmail.com>
2022-08-29 21:41:46 -0300

    inyecto en el constructor de cars controllers carsService

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,18 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
 import { Controller, Get, Param } from '@nestjs/common';
-
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-  private cars = ['Toyota', 'Volkswagen', 'Peugeot'];
+  constructor(private readonly carsService: CarsService) {}
 
   @Get()
-  getAllcars() {
-    return this.cars;
+  async getAllcars(carsService: CarsService) {
+    return this.carsService.findAll();
   }
-  
-  @Get(':id')
-  getCarById(@Param('id') id: string) {
-    return this.cars[id]
-  }
+
+  //@Get(':id')
+  //getCarById(@Param('id') id: string) {
+
+  //}
 }
---
+++ src/cars/cars.service.ts
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+  private cars = [
+    {
+      id: 1,
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: 2,
+      brand: 'Honda',
+      model: 'Civic',
+    },
+    {
+      id: 3,
+      brand: 'Jeep',
+      model: 'Cherokee',
+    },
+  ];
+
+  findAll() {
+    return this.cars;
+  }
+
+}
