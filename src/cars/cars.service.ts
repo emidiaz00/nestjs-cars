6adc408b861e966f01af116b9a27acd7dc6fa12c
emidiaz00 <emildsn00@gmail.com>
2022-09-05 21:05:48 -0300

    agrego metodo delete

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,9 +1,8 @@
 /* eslint-disable prettier/prettier */
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { v4 as uuid } from 'uuid'
+import { v4 as uuid } from 'uuid';
 import { Car } from './interfaces/car.interface';
 import { CreateCarDto, UpdateCarDto } from './dto';
-
 
 @Injectable()
 export class CarsService {
@@ -28,31 +27,42 @@ export class CarsService {
   async findAll() {
     return this.cars;
   }
-  findOneById
-  (
-    id: string
-  ) {
+  findOneById(id: string) {
     const car = this.cars.find((car) => car.id === id);
     if (!car) {
       throw new NotFoundException(`Car with id ${id} not found`);
     }
     return car;
   }
-  createCar( 
-  createCarDto: CreateCarDto 
-  ) {
+  createCar(createCarDto: CreateCarDto) {
     const car: Car = {
       id: uuid(),
-      ...createCarDto
-    }
-    this.cars.push(car)
+      ...createCarDto,
+    };
+    this.cars.push(car);
     return car;
   }
 
-  updateCar(
-    id: string,
-    updateCarDto: UpdateCarDto
-  ) {
+  updateCar(id: string, updateCarDto: UpdateCarDto) {
+    let carDB = this.findOneById(id);
 
+    this.cars = this.cars.map((car) => {
+      if (car.id === id) {
+        carDB = {
+          // esparso las propiedades de car, update and id
+          ...carDB,
+          ...updateCarDto,
+          id,
+        };
+        return carDB;
+      }
+      return car;
+    });
+    return carDB;
+  }
+
+  deleteCar(id: string) {
+    const car = this.findOneById(id);
+    this.cars = this.cars.filter(car => car.id !== id)
   }
 }
